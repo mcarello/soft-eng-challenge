ea6c2f70d3a4056128f569bde8994deda4205d8a
mcarello <mariano.carello@gmail.com>
2022-06-01 08:57:26 -0300

    Update API

--- alien_api/urls.py
+++ alien_api/urls.py
@@ -27,7 +27,7 @@ schema_view = get_schema_view(
       default_version='v1',
       description="Alien API for flugel-it challenge",
       terms_of_service="",
-      contact=openapi.Contact(email="alienapi@xxx.com"),
+      contact=openapi.Contact(email="mariano.carello@gmail.com"),
       license=openapi.License(name=""),
    ),
    public=True,
@@ -37,6 +37,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),   
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),   
 ]
